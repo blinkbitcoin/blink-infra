f83fea170c168995a81d8371f6f00b00398607b0
k9ert <117085+k9ert@users.noreply.github.com>
2025-05-20 16:11:18 +0200

    fix: config email

--- ci/tasks/helpers.sh
+++ ci/tasks/helpers.sh
@@ -105,10 +105,10 @@ function make_commit() {
   echo "    --> git user config"
 
   if [[ -z $(git config --global user.email) ]]; then
-    git config --global user.email "bot@galoy.io"
+    git config --global user.email "202112752+blinkbitcoinbot@users.noreply.github.com"
   fi
   if [[ -z $(git config --global user.name) ]]; then
-    git config --global user.name "CI Bot"
+    git config --global user.name "CI blinkbitcoinbot"
   fi
 
   echo "    --> git merge (${BRANCH}) + commit -m '${1}'"
