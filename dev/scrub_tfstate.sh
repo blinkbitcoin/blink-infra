b01625ea76d7daad7e4606f0fc00f82a79f9ae49
k9ert <117085+k9ert@users.noreply.github.com>
2025-06-26 16:01:24 +0200

    Update dev/scrub_tfstate.sh

--- dev/scrub_tfstate.sh
+++ dev/scrub_tfstate.sh
@@ -2,7 +2,7 @@
 set -eu
 
 TEMP_DIR=$(mktemp -d)
-#trap "echo '    --> removing $TEMP_DIR' && rm -rf $TEMP_DIR" EXIT
+trap "echo '    --> removing $TEMP_DIR' && rm -rf $TEMP_DIR" EXIT
 
 echo "    --> Cloning blink-infra-bootstrap-tfstate into $TEMP_DIR"
 git clone --depth 3 --single-branch --branch main --no-tags git@github.com:blinkbitcoin/blink-infra-bootstrap-tfstate.git "$TEMP_DIR/blink-infra-bootstrap-tfstate"
