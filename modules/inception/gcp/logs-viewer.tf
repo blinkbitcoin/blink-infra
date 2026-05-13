083d39524a8a332ff2c943e7a04aa6903eaf3033
openoms <43343391+openoms@users.noreply.github.com>
2026-05-13 12:23:28 +0200

    fix: add access to custom log bucket (#30)

--- modules/inception/gcp/logs-viewer.tf
+++ modules/inception/gcp/logs-viewer.tf
@@ -4,3 +4,10 @@ resource "google_project_iam_member" "dev_read_log" {
   role     = "roles/logging.viewer"
   member   = each.key
 }
+
+resource "google_project_iam_member" "dev_read_log_views" {
+  project  = local.project
+  for_each = toset(local.log_viewers)
+  role     = "roles/logging.viewAccessor"
+  member   = each.key
+}
