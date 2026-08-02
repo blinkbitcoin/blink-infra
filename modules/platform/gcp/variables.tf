9f491f394e4c8ed38c459600e08c1588fb0a820c
CI blinkbitcoinbot <202112752+blinkbitcoinbot@users.noreply.github.com>
2026-08-02 16:04:48 +0000

    chore: bump kubernetes to '1.33.13-gke.1011000'

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -10,7 +10,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "1.33.12-gke.1270000"
+  default = "1.33.13-gke.1011000"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-4"
